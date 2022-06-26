12d5a6478d108112c0bd9609943fd44a8daa5c55
April <buzdav2@gmail.com>
2022-06-26 18:17:55 -0500

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,125 @@
+#include "GL/glew.h"
+#include "SDL.h"
+
+char const *VERTEX_SHADER_SOURCE = R"(
+    #version 330 core
+    layout (location = 0) in vec3 aPos;
+    void main() {
+        gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    }
+)";
+
+char const *FRAGMENT_SHADER_SOURCE = R"(
+    #version 330 core
+    out vec4 fragColor;
+
+    void main() {
+        fragColor = vec4(1.0, 0.5, 0.2, 1.0);
+    }
+)";
+
+int main(int argc, char *argv[]) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        SDL_Log("Error initializing SDL: %s", SDL_GetError());
+        return 1;
+    }
+
+    if (SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3) != 0) {
+        SDL_Log("Error setting OpenGL major version: %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+    if (SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3) != 0) {
+        SDL_Log("Error setting OpenGL minor version: %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("hello", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 400, SDL_WINDOW_OPENGL);
+    if (window == nullptr) {
+        SDL_Log("Error creating SDL window: %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+
+    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+    if (gl_context == nullptr) {
+        SDL_Log("Error creating OpenGL context: %s", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    GLenum glew_init_result = glewInit();
+    if (glew_init_result != GLEW_OK) {
+        SDL_Log("Error initializing GLEW: %s", glewGetErrorString(glew_init_result));
+        SDL_GL_DeleteContext(gl_context);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    GLfloat vertices[] = {
+            -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f,  0.5f, 0.0f
+    };
+
+
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+
+    GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &VERTEX_SHADER_SOURCE, nullptr);
+    glCompileShader(vertex_shader);
+
+    GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &FRAGMENT_SHADER_SOURCE, nullptr);
+    glCompileShader(fragment_shader);
+
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    glLinkProgram(shader_program);
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+
+
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * sizeof(GLfloat), static_cast<void*>(0));
+    glEnableVertexAttribArray(0);
+
+    bool running = true;
+    while (running) {
+        SDL_Event e;
+        while (SDL_PollEvent(&e)) {
+            switch (e.type) {
+                case SDL_QUIT: {
+                    running = false;
+                    break;
+                }
+            }
+        }
+
+        glBindVertexArray(vao);
+        glUseProgram(shader_program);
+        glDrawArrays(GL_POINTS, 0, 3);
+
+        SDL_GL_SwapWindow(window);
+
+        SDL_Delay(1000/60);
+    }
+
+    SDL_GL_DeleteContext(gl_context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
